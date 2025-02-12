cf4c149a8dc79d5eb7c94bf7a559578b7cdcb1a0
Boris Terzic <boris@terzic.be>
2025-02-12 09:01:47 +0100

    Updated dependencies Added a CSRF middleware that verifies the origin header against the target origin for relevant HTTP methods

---
+++ middleware/middleware.go
@@ -0,0 +1,47 @@
+package middleware
+
+import (
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+)
+
+func CsrfMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		// CSRF check unnecessary for GET and HEAD requests as they are safe and idempotent and the Origin header won't be available anyway
+		if c.Request().Method == "HEAD" || c.Request().Method == "GET" {
+			return next(c)
+		}
+		originHeader := c.Request().Header.Get("Origin")
+		hostHeader := c.Request().Host
+		// parse the target origin from the host header and the X-Forwarded-Host header when present
+		hostParts := strings.Split(hostHeader, ":")
+		hostName := hostParts[0]
+		targetOriginPort := hostParts[1]
+		targetOriginHostname := c.Request().Header.Get("X-Forwarded-Host")
+		if targetOriginHostname == "" {
+			targetOriginHostname = hostName
+		}
+		// parse the hostname and the port from the Origin header
+		parsedURL, err := url.Parse(originHeader)
+		if err != nil {
+			return err
+		}
+		originHostname := parsedURL.Hostname()
+		originPort := "80"
+		if parsedURL.Port() != "" {
+			originPort = parsedURL.Port()
+		}
+		if originHostname != targetOriginHostname || originPort != targetOriginPort {
+			c.Logger().Info("CSRF check failed: Origin does not match target origin",
+				"originHostname", originHostname,
+				"targetOriginHostname", targetOriginHostname,
+				"originPort", originPort,
+				"targetOriginPort", targetOriginPort)
+			return echo.NewHTTPError(http.StatusForbidden, "forbidden")
+		}
+		return next(c)
+	}
+}
--- oidc/session.go
+++ oidc/session.go
@@ -2,11 +2,9 @@ package oidc
 
 import (
 	"encoding/base64"
-	"errors"
 	"log"
 	"net/http"
 	"strings"
-	"time"
 
 	"github.com/coreos/go-oidc/v3/oidc"
 	"github.com/gorilla/sessions"
@@ -14,70 +12,70 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-var sessionCookieName = "baselib-oidc-session-cookie"
+var SessionCookieName = "baselib-oidc-session-cookie"
 
-func GetUserIdFromSession(c echo.Context) (int, error) {
-	sess, err := session.Get(sessionCookieName, c)
-	if err != nil {
-		return 0, err
-	}
-	if sess.Values["userid"] != nil {
-		return sess.Values["userid"].(int), nil
-	} else {
-		return 0, errors.New("no userid in session")
-	}
-}
+// func GetUserIdFromSession(c echo.Context) (int, error) {
+// 	sess, err := session.Get(sessionCookieName, c)
+// 	if err != nil {
+// 		return 0, err
+// 	}
+// 	if sess.Values["userid"] != nil {
+// 		return sess.Values["userid"].(int), nil
+// 	} else {
+// 		return 0, errors.New("no userid in session")
+// 	}
+// }
 
-func clearSessionCookie(c echo.Context) {
-	c.SetCookie(&http.Cookie{
-		Name:     sessionCookieName,
-		Value:    "",
-		Path:     "/", // TODO: this path is not context path safe
-		Expires:  time.Unix(0, 0),
-		HttpOnly: true,
-	})
-}
+// func clearSessionCookie(c echo.Context) {
+// 	c.SetCookie(&http.Cookie{
+// 		Name:     sessionCookieName,
+// 		Value:    "",
+// 		Path:     "/", // TODO: this path is not context path safe
+// 		Expires:  time.Unix(0, 0),
+// 		HttpOnly: true,
+// 	})
+// }
 
-func IsAuthenticated(c echo.Context) bool {
-	userId, err := GetUserIdFromSession(c)
-	if err != nil && userId != 0 {
-		return true
-	} else {
-		clearSessionCookie(c)
-		return false
-	}
-}
+// func IsAuthenticated(c echo.Context) bool {
+// 	userId, err := GetUserIdFromSession(c)
+// 	if err != nil && userId != 0 {
+// 		return true
+// 	} else {
+// 		clearSessionCookie(c)
+// 		return false
+// 	}
+// }
 
-func CreateSessionBasedOidcDelegate(resolveUsername func(username string) (int, error), fallbackRedirectUrl string) func(c echo.Context, idToken *oidc.IDToken, state string) error {
-	return CreateSessionBasedOidcDelegateWithClaims(resolveUsername, fallbackRedirectUrl, nil)
-}
-
-func CreateSessionBasedOidcDelegateWithClaims(resolveUsername func(username string) (int, error), fallbackRedirectUrl string, claimsFactory func() interface{}) func(c echo.Context, idToken *oidc.IDToken, state string) error {
+func CreateSessionBasedOidcDelegate(handleIdToken func(c echo.Context, idToken *oidc.IDToken) error, fallbackRedirectUrl string) func(c echo.Context, idToken *oidc.IDToken, state string) error {
 	return func(c echo.Context, idToken *oidc.IDToken, state string) error {
-		// we now have a valid ID token, to progress in the application we need to map this
-		// to an existing user or create a new one on demand
-		username := idToken.Subject
-		userId, err := resolveUsername(username)
+		err := handleIdToken(c, idToken)
 		if err != nil {
-			log.Println("Error retrieving or creating user: ", err)
 			return c.Render(http.StatusInternalServerError, "error-internal", nil)
 		}
-		// we have a valid user, we can now create a session and redirect to the original request
-		sess, _ := session.Get(sessionCookieName, c)
-		sess.Values["userid"] = userId
-		if claimsFactory != nil {
-			claims := claimsFactory()
-			if err := idToken.Claims(&claims); err != nil {
-				log.Println("Error retrieving claims: ", err)
-				return c.Render(http.StatusInternalServerError, "error-internal", nil)
-			}
-			sess.Values["userclaims"] = claims
-		}
-		err = sess.Save(c.Request(), c.Response())
-		if err != nil {
-			log.Println(err)
-			return c.Render(http.StatusInternalServerError, "error-internal", nil)
-		}
+		// // we now have a valid ID token, to progress in the application we need to map this
+		// // to an existing user or create a new one on demand
+		// username := idToken.Subject
+		// user, err := resolveUsername(username)
+		// if err != nil {
+		// 	log.Println("Error retrieving or creating user: ", err)
+		// 	return c.Render(http.StatusInternalServerError, "error-internal", nil)
+		// }
+		// // we have a valid user, we can now create a session and redirect to the original request
+		// sess, _ := session.Get(sessionCookieName, c)
+		// sess.Values["user"] = user
+		// if claimsFactory != nil {
+		// 	claims := claimsFactory()
+		// 	if err := idToken.Claims(&claims); err != nil {
+		// 		log.Println("Error retrieving claims: ", err)
+		// 		return c.Render(http.StatusInternalServerError, "error-internal", nil)
+		// 	}
+		// 	sess.Values["userclaims"] = claims
+		// }
+		// err = sess.Save(c.Request(), c.Response())
+		// if err != nil {
+		// 	log.Println(err)
+		// 	return c.Render(http.StatusInternalServerError, "error-internal", nil)
+		// }
 		stateParts := strings.Split(state, "|")
 		if len(stateParts) > 1 {
 			originalRequestUrlBase64 := stateParts[1]
@@ -89,7 +87,7 @@ func CreateSessionBasedOidcDelegateWithClaims(resolveUsername func(username stri
 			return c.Redirect(http.StatusFound, string(decodedOriginalRequestUrl))
 		} else {
 			// this is just for robustness, if the state is valid, but does not contain a redirect URL
-			// we just go to the bookmarks page
+			// we just go to the fallback URL
 			return c.Redirect(http.StatusFound, fallbackRedirectUrl)
 		}
 	}
@@ -97,7 +95,7 @@ func CreateSessionBasedOidcDelegateWithClaims(resolveUsername func(username stri
 
 // SetFlash sets a flash message with a given key (e.g., "success", "error")
 func SetFlash(c echo.Context, key, message string) error {
-	sess, err := session.Get(sessionCookieName, c)
+	sess, err := session.Get(SessionCookieName, c)
 	if err != nil {
 		return err
 	}
@@ -108,7 +106,7 @@ func SetFlash(c echo.Context, key, message string) error {
 
 // GetFlashes retrieves and clears flash messages for the given keys
 func GetFlashes(c echo.Context) ([]string, []string, error) {
-	session, err := session.Get(sessionCookieName, c)
+	session, err := session.Get(SessionCookieName, c)
 	if err != nil {
 		return nil, nil, err
 	}
